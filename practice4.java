fa9f36b63cc3c642369678ed2d30e2523e4c3f30
forroleves <41123024+forroleves@users.noreply.github.com>
2018-10-29 15:33:44 +0100

    practice4.java
    practice4.java

---
+++ practice4.java
@@ -0,0 +1,8 @@
+
+class IntroduceYourself {
+    public static void main(String[] args) {
+       System.out.println("Tamas Nagy");
+       System.out.println(25);
+       System.out.println(1.85);
+    }
+}
